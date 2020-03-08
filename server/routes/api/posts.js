42ff6912d7889438e3c350d245e0daf72a6245ac
James <lanejames35@gmail.com>
2020-03-08 00:13:38 -0500

    Convert MongoDB driver to mongoose

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -2,12 +2,12 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const Post = require("../../models/post");
 
 // Get posts
 router.get("/", async (req, res) => {
   try{
-    const posts = await loadPostsCollection();
-    res.send( await posts.find({}).toArray() );
+    res.send( await Post.find({}) );
   } catch(err){
     console.log(err);
   }
@@ -15,9 +15,8 @@ router.get("/", async (req, res) => {
 // Get post by ID
 router.get("/:id", async (req, res) => {
   try{
-    const posts = await loadPostsCollection();
-    res.send( await posts.findOne({ 
-      _id: new mongodb.ObjectID(req.params.id)
+    res.send( await Post.findById({ 
+      _id: new mongoose.Types.ObjectId(req.params.id)
     }) );
   } catch(err){
     console.log(err);
@@ -27,11 +26,12 @@ router.get("/:id", async (req, res) => {
 // Post a post
 router.post("/", async (req, res) => {
   try{
-    const posts = await loadPostsCollection();
-    await posts.insertOne({
+    await new Post({
       title: req.body.title,
-      text: req.body.text,
-      createdAt: new Date()
+      tags: req.body.tags,
+      author: new mongoose.Types.ObjectId(req.body.author),
+      created: new Date(),
+      body: req.body.body
     });
   } catch(err){
     console.log(err);
@@ -42,28 +42,11 @@ router.post("/", async (req, res) => {
 // Delete posts
 router.delete("/:id", async (req, res) => {
   try{
-    const posts = await loadPostsCollection();
-    await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+    await Post.deleteOne({ _id: new mongoose.Types.ObjectId(req.params.id) });
   } catch(err){
     console.log(err);
   }
   res.status(200).send();
 })
-
-// Load posts from storage
-async function loadPostsCollection(){
-  try{
-    const client = await mongoose.connect(
-      "mongodb+srv://dbMaster:Senators123@cluster0-nkbbk.azure.mongodb.net/test?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
-    );
-    return client.collection("posts");
-  } catch(err){
-    console.log(err);
-  }
-}
 
 module.exports = router;
